b4be808099ee38c105163aee1082a8268c155971
Jarory <jarory13@gmail.com>
2019-07-02 17:57:50 -0400

    Stand up web app

--- gke_mon_test_webapp/server.js
+++ gke_mon_test_webapp/server.js
@@ -8,9 +8,28 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
+
+// router.get('/about',function(req,res){
+//   res.sendFile(path.join(__dirname+'/about.html'));
+// });
+
+// router.get('/sitemap',function(req,res){
+//   res.sendFile(path.join(__dirname+'/sitemap.html'));
+// });
+
+//add the router
+app.use('/', router);
+//app.listen(process.env.port || 3000);
+
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
