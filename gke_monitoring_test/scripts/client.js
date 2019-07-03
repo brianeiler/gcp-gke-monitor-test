1c20a286d9b818bf4181b1f579911384661e489d
Brian Eiler <brianeiler@gmail.com>
2019-07-02 22:47:43 -0400

    Added Sandbox Version

---
+++ gke_monitoring_test/scripts/client.js
@@ -0,0 +1,103 @@
+
+var loadButton = null;
+var data = null;
+var running = false;
+var loaded = function()
+{
+	//Grab our loaded vars
+	loadButton = document.getElementById("loadButton");	
+	toggle = document.getElementById("onOffToggle");	
+	
+	//If our JSON isn't yet loaded, load it
+	if(data == null)
+	{
+	 	loadJSON(function(json) {
+	  			console.log(json); // this will log out the json object
+	  			data = json;
+	  			console.log(data.isRunning)
+	  			running = (data.isRunning == "true" || data.isRunning == true) ? true : false ;
+
+	  			console.log(running);
+	if(running)
+	  	{
+			
+			toggle.checked = true;
+			loadButton.disabled = true;
+			DoWhileRunning();
+		}
+	else
+	{
+		toggle.checked = false;
+		loadButton.disabled = false;
+			//Add funcitonality to the generate load button
+	assignLoadButton();
+	}
+
+		});
+	}
+
+}
+
+
+function loadJSON(callback) {   
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', './Scripts/data.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(JSON.parse(xobj.responseText));
+    }
+  };
+  xobj.send(null);  
+}
+
+
+//TODO: While running, check to see if we have stopped running
+var DoWhileRunning = function()
+{
+	//tempCode
+	setTimeout(StopRunning, 1000);
+}
+
+//Begin the running process
+var StartRunning = function()
+{
+	data.isRunning = true;
+	JSON.stringify(data);
+	PostFunction(data);
+	DoWhileRunning();
+}
+
+var StopRunning = function()
+{
+	toggle.checked = false;
+	loadButton.disabled = false;
+	data.isRunning = false;
+	JSON.stringify(data);
+	PostFunction(data);
+	assignLoadButton();
+
+}
+
+var assignLoadButton = function() 
+{
+	running = (data.isRunning == "true" || data.isRunning == true) ? true : false ;
+	loadButton.onclick = function(){
+	  			
+		if(!running && data != null)
+		{
+			//Launch things
+			StartRunning();
+			toggle.checked = true;
+			loadButton.disabled = true;
+		}
+	}
+}
+
+var PostFunction = function(myData)
+{
+	const url = "http://localhost:8080/CPU_On";
+	$.post(url, myData, function(dataBack, status){
+		console.log(dataBack)
+	}, 'json');
+}
---
+++ gke_monitoring_test/server.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var dataFilePath = './Scripts/data.json'
+var JSONData = require( dataFilePath);
+var http = require('http');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+
+function getJson(req, res, next){
+    res.send(data);
+}
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+const path = require('path');
+const router = express.Router();
+var server = http.createServer(app);
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+// router.get('/about',function(req,res){
+//   res.sendFile(path.join(__dirname+'/about.html'));
+// });
+
+// router.get('/sitemap',function(req,res){
+//   res.sendFile(path.join(__dirname+'/sitemap.html'));
+// });
+
+//add the router
+// app.use(express.static(__dirname + '/View'));
+//Store all HTML files in view folder.
+app.use("/Scripts", express.static(__dirname + '/Scripts'));
+//Store all JS and CSS in Scripts folder.
+
+app.use('/', router);
+// app.listen(process.env.port || 3000);
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// console.log('Running at Port 3000');
+app.post('/CPU_On', function(req, res) {
+  console.log(req.body);
+  var isRunning = req.body.isRunning;
+  console.log(isRunning);
+  JSONData.isRunning = isRunning;
+  fs.writeFile(dataFilePath, JSON.stringify(JSONData, null, 2), errorHandler);
+  res.sendStatus(200);
+});
+
+
+var errorHandler = function()
+{
+	//TODO
+}
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
