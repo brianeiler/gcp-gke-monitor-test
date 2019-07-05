bfad222008605f65235206056556910c572c1068
Brian Eiler <brianeiler@gmail.com>
2019-07-05 13:17:33 -0400

    Update client.js

--- scripts/client.js
+++ scripts/client.js
@@ -1,11 +1,71 @@
 
-var loadButton = null;
-var data = null;
-var running = false;
-var loaded = function()
-{
-	//Grab our loaded vars
-	loadButton = document.getElementById("loadButton");	
+var JSONData = null;
+var cpuLoadRunning = false;
+var customMetricCreated = false;
+var userCount = 0;
+var debugMode = false;
+
+var loaded = function() {
+	// --------------------------------------------------------------------------------------
+	// SECTION: Initialization
+	// This code sets configuration values and retrieves the server state from a JSON file.
+	// --------------------------------------------------------------------------------------
+	//
+
+	// Retrieves the server-side data.json file via HTTP and parses its current values into variables.
+	loadJSON(function(JSONData) {
+		cpuLoadRunning      = JSONData.CpuIsRunning;
+		customMetricCreated = JSONData.CustomMetricCreated;
+		userCount           = JSONData.UserCount;
+		debugMode           = JSONData.DebugMode;
+	});
+
+	// Set the CPU Load Generator document elements to objects
+	var btnStartCPU   = document.getElementById('btnStartCPU');
+	var btnStopCPU    = document.getElementById('btnStopCPU');
+	var lblCPUStatus  = document.getElementById('lblCPUStatus');	// Maps to the JSON cpuStatus Label for CPU Load Generator
+	
+	// Set the Custom Metric document elements to objects
+	var btnStartMonitoring    = document.getElementById('btnStartMonitoring');
+	var btnIncreaseUserCount  = document.getElementById('btnIncreaseUserCount');
+	var btnDecreaseUserCount  = document.getElementById('btnDecreaseUserCount');
+	var lblCustomMetricStatus = document.getElementById('lblCustomMetricStatus');	// Maps to the JSON customMetricCreated flag
+	var lblCurrentUserCount   = document.getElementById('lblCurrentUserCount');		// Maps to the JSON userCount value
+
+	// --------------------------------------------------------------------------------------
+	// SECTION: Staging
+	// This code sets the document controls and status text based on the variables set above.
+	// --------------------------------------------------------------------------------------
+	//
+	// CPU Load Generator Section
+	//
+	if (cpuLoadRunning) {
+		// CPU Load Generator is running, so disable the START button and set the status to "Running"
+		btnStartCPU.disabled   = true;
+		btnStopCPU.disabled    = false;
+		lblCPUStatus.innerHTML = "CPU load RUNNING";
+	}
+	else {
+		// CPU isn't running yet, so disable the stop button and set the status to "Not Running"
+		btnStartCPU.disabled   = false;
+		btnStopCPU.disabled    = true;
+		lblCPUStatus.innerHTML = "CPU load NOT running";
+	}	
+	//
+	// Custom Metric Section
+	//
+	if (customMetricCreated) {
+		// CPU Load Generator is running, so disable the START button and set the status to "Running"
+		btnStartCPU.disabled   = true;
+		btnStopCPU.disabled    = false;
+		lblCPUStatus.innerHTML = "CPU load RUNNING";
+	}
+	else {
+		// CPU isn't running yet, so disable the stop button and set the status to "Not Running"
+		btnStartCPU.disabled   = false;
+		btnStopCPU.disabled    = true;
+		lblCPUStatus.innerHTML = "CPU load NOT running";
+	}	
 	
 	//If our JSON isn't yet loaded, load it
 	if(data == null)
@@ -15,16 +75,15 @@ var loaded = function()
 	  			data = json;
 	  			console.log(data.isRunning)
 	  			running = (data.isRunning == "true" || data.isRunning == true) ? true : false ;
-
 	  			console.log(running);
-	if(running)
-	  	{
-			
-			toggle.checked = true;
-			loadButton.disabled = true;
-			DoWhileRunning();
-		}
-	else
+				if(running)
+					{
+						
+						toggle.checked = true;
+						loadButton.disabled = true;
+						DoWhileRunning();
+					}
+				else
 	{
 		toggle.checked = false;
 		loadButton.disabled = false;
